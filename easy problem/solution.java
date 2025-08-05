c31259ce17475dc368b05fac6052cc325b00cc8b
Ssakthiiswarya <sakthiiswaryas@gmail.com>
2025-08-05 16:00:58 +0530

    Update solution.java

--- easy problem/solution.java
+++ easy problem/solution.java
@@ -68,3 +68,81 @@ class Solution {
     }
 }
 ---------------------------------------------------------------------------------------------------------------------
+//FLOOR IN A SORTED ARRAY(SMALLER NO)//
+import java.util.*;
+public class Solution {
+    public static int findFloor(int[] arr, int x) {
+        int n = arr.length;
+        int floor = -1;
+        for (int i = 0; i < n; i++) {
+            if (arr[i] <= x) {
+                floor = i; 
+            } else {
+                break; 
+            }
+        }
+        return floor;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt(); 
+        int x = sc.nextInt(); 
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        
+        int output = findFloor(arr, x);
+        System.out.println(output);
+    }
+}
+---------------------------------------------------------------------------
+//ANAGRAM//
+class Solution {
+    public boolean areAnagrams(String s1, String s2) {
+        if (s1.length() != s2.length()) return false;
+
+        int[] count = new int[26];
+
+        for (int i = 0; i < s1.length(); i++) {
+            count[s1.charAt(i) - 'a']++;
+            count[s2.charAt(i) - 'a']--;
+        }
+
+        for (int c : count) {
+            if (c != 0) return false;
+        }
+
+        return true;
+    }
+}
+------------------------------------------------------------------------------------
+//PANAGRAM CHECKING//
+import java.util.*;
+
+public class Solution {
+    
+    public boolean checkPangram(String str) {
+        str = str.toLowerCase();
+        HashSet<Character> hs = new HashSet<>();
+
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+            if (ch >= 'a' && ch <= 'z') {
+                hs.add(ch);
+            }
+        }
+
+        return hs.size() == 26;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+
+        System.out.println(new Solution().checkPangram(s) ? "true" : "false");
+    }
+}
+
